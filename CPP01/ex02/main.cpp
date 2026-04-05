d46ce1f21a9bddba1ebc43cb7a3416a232dc096e
eganas76@gmail.com <eganas@XANA.localdomain>
2026-04-05 15:21:40 +0200

    CPP01 v0.2.1: ex02 Done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eganas <eganas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/05 12:38:20 by eganas            #+#    #+#             */
+/*   Updated: 2026/04/05 15:19:16 by eganas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iostream"
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "Memory Adress str:		" << &str << std::endl;
+    std::cout << "Memory Adress stringPTR:	" << stringPTR << std::endl;
+    std::cout << "Memory Adress stringREF:	" << &stringREF << std::endl;
+    
+    std::cout << "Value of str:			" << str << std::endl;
+    std::cout << "Value of stringPTR:		" << *stringPTR << std::endl;
+    std::cout << "Value of stringREF:		" << stringREF << std::endl;
+    return (0);
+}
