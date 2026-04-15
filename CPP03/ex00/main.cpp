c85843215c2d1ec9df34364675e9891a70c0b212
Anas El Ghazzani <ael-ghaz@c1r1p11.42lehavre.fr>
2026-04-15 15:44:48 +0200

    CPP03 v0.0.3: modify tests main

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eganas <eganas@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ael-ghaz <ael-ghaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 12:38:20 by eganas            #+#    #+#             */
-/*   Updated: 2026/04/13 01:49:38 by eganas           ###   ########.fr       */
+/*   Updated: 2026/04/15 15:39:34 by ael-ghaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ int main( void ) {
 
 	ct.attack("ESP27");
 	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
-	ct.attack("ESP27");
 	ct.takeDamage(3);
 	ct.beRepaired(2);
-
+	std::cout << std::endl;
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
+	ct.attack("ESP27");
 	ct.takeDamage(9);
 	ct.beRepaired(2);
 	ct.attack("ESP27");
