1aa9fc409b904050e57953858ce6114c88be8625
eganas76@gmail.com <eganas@XANA.localdomain>
2026-09-19 14:11:44 +0200

    CPP06 v2.0: ex02 done

---
+++ CPP06/ex02/src/main.cpp
@@ -0,0 +1,66 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cstdlib>
+#include <ctime>
+
+Base *generate(void)
+{
+	int randValue = std::rand() % 3;
+	if (randValue == 0)
+		return new A();
+	else if (randValue == 1)
+		return new B();
+	return new C();
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		A& a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+		
+	}
+	catch(const std::exception& e) {}
+
+	try
+	{
+		B& b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch(const std::exception& e) {}
+
+	try
+	{
+		C& c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch(const std::exception& e) {}
+}
+
+int main()
+{
+	srand(time(NULL));
+	Base *ptr = generate();
+	Base &ref = *ptr;
+
+	identify(ptr);
+	identify(ref);
+	delete ptr;
+	return 0;
+}
